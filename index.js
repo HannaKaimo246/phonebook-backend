e49ed8b8ca29cf5d7cd289aa55ad9ce431a14899
Hanna Kaimo <159903348+HannaKaimo246@users.noreply.github.com>
2024-07-15 11:46:57 +0300

    Update index.js

--- index.js
+++ index.js
@@ -114,7 +114,7 @@ app.post('/api/persons', (request, response) => {
   response.json(person)
 })
 
-const PORT = process.env.PORT || 3001
-app.listen(PORT, '0.0.0.0', () => {
-  console.log(`Server running on http://0.0.0.0:${PORT}`);
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
 })
